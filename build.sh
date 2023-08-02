a56306e5d7ab374688f33f10ec9ffd74af495c89
Eric Smith <keenfury@gmail.com>
2023-08-02 17:41:30 -0600

    Fixing docker build

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # make sure the sqlite data file is there
-if ![ -e "/data/agenda.sql" ]; then mkdir -p "/data"; touch "/data/agenda.sql"; fi
+if [ ! -e "/Users/ericsmith/data/agenda.sql" ]; then mkdir -p "/Users/ericsmith/data"; touch "/Users/ericsmith/data/agenda.sql"; fi
 
 # build and run docker
-docker build -t agenda.latest -f ./build/Dockerfile
+docker build -t agenda.latest -f ./build/Dockerfile .
 
 is_running=$(docker ps | grep agenda | wc -l)
 
 if [ "$is_running" != "1" ]; then docker stop agenda && docker rm agenda; fi
 
-docker run -d --name=agenda -v /data:/app/data  --env_file ./env_vars agenda.latest
+docker run -d --name=agenda -v /data:/app/data  --env-file ./env_vars agenda.latest
